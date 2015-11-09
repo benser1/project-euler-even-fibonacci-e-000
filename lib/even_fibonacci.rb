1be499c4676e5cf08710eedd3e9de418d2cbe86b
benser1 <briganenser28@gmail.com>
2015-11-09 14:56:24 -0700

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,11 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(bound)
+  n = 2
+  old = 1
+  set = [1,2]
+  while n + old < bound
+    old, n = n, old + n
+    set << n
+  end
+  set.select{|num| num%2 == 0}.reduce(:+)
+end
